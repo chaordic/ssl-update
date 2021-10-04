73d85a6811f73ac939988e166fd13067ac909058
Alessandro Elias <alessandro.elias@linx3.com>
2021-10-04 14:19:17 -0300

    Generic function to import certificates

--- hooks/default.sh
+++ hooks/default.sh
@@ -1,14 +1,43 @@
+##
+# overwrite this var if you prefer to use a different user
+# than the global one
+#
+# SSH_USER="<my prefered user>"
+
+
+DST_DIR="/etc/nginx/ssl"
+CERT_FILE="STAR_chaordicsystems_com.ca_ssl_bundle"
+PRIV_FILE="STAR_chaordicsystems_com.key"
+
 import_certificate() {
-    set -x
     local fullchain_content="$1"
     local privkey_content="$2"
-
-    local dst_dir="/mnt/certs/"
-    local container_name="platform-registry"
-    local host="docker-registry.chaordicsystems.com"
+    local extended="$3"
     local ssh_output=""
 
-    echo "import_cert()" 1>&2
-    set +x
-    return 0
+    if [ -z "$SSH_USER" ]; then
+        echo "SSH_USER variable has not been defined!\nAborting renew." 1>&2
+        return 1
+    fi
+
+    ssh_output=$(ssh -T -o IdentitiesOnly=yes -o PreferredAuthentications=publickey $SSH_USER@$extended "
+sudo su -- <<'EOFSU'
+pushd $DST_DIR &>/dev/null;\
+cp -a $CERT_FILE ${CERT_FILE}.expired;\
+cp -a $PRIV_FILE ${PRIV_FILE}.expired;\
+cat <<'EOF' > $CERT_FILE
+$fullchain_content
+EOF
+cat <<'EOF' > $PRIV_FILE
+$privkey_content
+EOF
+chown root:www-data $CERT_FILE;\
+chown root:www-data $PRIV_FILE;\
+chmod 0640 $PRIV_FILE;\
+popd &>/dev/null;\
+nginx -s reload
+echo \$?;
+EOFSU
+")
+    return $(tail -1 <<<"$ssh_output")
 }
